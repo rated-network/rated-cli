714046bcd2277da2fdcc47986f491cda44b9e594
mxs <mxs@sbrk.org>
2022-05-22 17:48:29 +0200

    feat: implement main watching loop

--- pkg/watcher/watcher.go
+++ pkg/watcher/watcher.go
@@ -1,6 +1,8 @@
 package watcher
 
 import (
+	"time"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/skillz-blockchain/rated-cli/pkg/core"
 )
@@ -23,5 +25,23 @@ func NewWatcher(cfg *core.Config) *Watcher {
 }
 
 func (w *Watcher) Watch() error {
+	log.Info("starting to watch keys")
+
+	for {
+		startAt := time.Now()
+		nextAt := startAt.Add(time.Duration(w.cfg.WatcherRefreshRate))
+		log.WithFields(log.Fields{
+			"start-at": startAt,
+			"next-at": nextAt,
+		}).Info("starting new iteration")
+
+		sleepFor := time.Until(nextAt)
+		log.WithFields(log.Fields{
+			"sleep-for": sleepFor,
+		}).Info("sleeping until next iteration")
+		time.Sleep(sleepFor)
+		log.Info("end of iteration")
+	}
+
 	return nil
 }
