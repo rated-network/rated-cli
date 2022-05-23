558d1c3b166e5847a6bbc2c3efac525d47177492
mxs <mxs@sbrk.org>
2022-05-23 09:40:05 +0200

    feat: add new configuration for the local service exposed

--- pkg/core/config.go
+++ pkg/core/config.go
@@ -8,6 +8,7 @@ import (
 type Config struct {
 	ApiEndpoint           string        // endpoint to Rated network API
 	BeaconEndpoint        string        // endpoint to a beacon (lighthouse, teku, ...)
+	ListenOn	      string        // interface to listen on to expose statistics
 	WatcherRefreshRate    time.Duration // refresh rate of the watcher
 	WatcherValidationKeys []string      // validation keys to watch
 }
