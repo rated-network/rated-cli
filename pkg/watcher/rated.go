8a40ac5e0d6c429d5f6c5cbac4b48058270526bb
ariskk <aris@ariskk.com>
2022-06-15 17:20:56 +0300

    Implement exponential backoff

--- pkg/watcher/rated.go
+++ pkg/watcher/rated.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math"
+	"math/rand"
 	"net/http"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/skillz-blockchain/rated-cli/pkg/core"
@@ -25,83 +28,122 @@ type getValidatorEffectivenessData struct {
 	ValidatorEffectiveness float64 `json:"validatorEffectiveness"`
 }
 
-// getValidationStatistics fetches data about a validator on rated network.
 func getValidationStatistics(cfg *core.Config, key string) (*getValidatorEffectivenessData, error) {
-	// Key here is already prefixed with '0x', this is required for rated
-	// network API (which also supports indexes).
+
 	url := fmt.Sprintf("%s/v0/eth/validators/%s/effectiveness?size=1", cfg.ApiEndpoint, key)
 
 	log.WithFields(log.Fields{
 		"url":            url,
 		"validation-key": key,
-	}).Info("fetching rated data for validation key")
+	}).Info("Fetching rated data for validation key")
 
-	res, err := http.Get(url)
-	if err != nil {
-		log.WithError(err).WithFields(log.Fields{
-			"url":            url,
-			"validation-key": key,
-		}).Warn("unable to fetch data about validation key from rated network")
+	maxRetries := 10
 
-		return nil, err
-	}
-	defer res.Body.Close()
+	for r := 0; r <= maxRetries; r++ {
 
-	log.WithFields(log.Fields{
-		"url":            url,
-		"validation-key": key,
-	}).Info("reading response from rated  network")
+		res, err := http.Get(url)
 
-	if res.StatusCode != 200 {
-		log.WithFields(log.Fields{
-			"url":         url,
-			"status-code": res.StatusCode,
-		}).Warn("unable to fetch data about this validation key")
+		if err != nil {
+			log.WithError(err).WithFields(log.Fields{
+				"url":            url,
+				"validation-key": key,
+			}).Warn("Unable to fetch validator data. Aborting")
 
-		return nil, fmt.Errorf("non 200-response code received from rated network")
-	}
+			// Abort on unknown error
+			return nil, err
+		}
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.WithError(err).WithFields(log.Fields{
-			"url":            url,
-			"validation-key": key,
-		}).Warn("unable to read rated network http body")
+		defer res.Body.Close()
 
-		return nil, err
-	}
-
-	log.WithFields(log.Fields{
-		"url":            url,
-		"validation-key": key,
-	}).Info("parsing response from rated network")
-
-	var response getValidationEffectiveness
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		log.WithError(err).WithFields(log.Fields{
-			"url":            url,
-			"validation-key": key,
-		}).Warn("unable to parse rated network http body into expected response")
-
-		return nil, err
-	}
-
-	if len(response.Data) != 1 {
 		log.WithFields(log.Fields{
 			"url":            url,
-			"status-code":    res.StatusCode,
 			"validation-key": key,
-			"nb-entries":     len(response.Data),
-		}).Warn("expected 1 entry of statistics for the validation key")
+		}).Info("Reading response from rated network")
 
-		return nil, fmt.Errorf("unexpected data response from rated network")
+		if res.StatusCode == 500 {
+			log.WithFields(log.Fields{
+				"url":           url,
+				"status-code":   res.StatusCode,
+				"validator-key": key,
+			}).Error("Unable to fetch validator data due to internal server error. Aborting")
+
+			return nil, fmt.Errorf("Internal server error received from rated network")
+		}
+
+		if res.StatusCode == 404 {
+			log.WithFields(log.Fields{
+				"url":            url,
+				"status-code":    res.StatusCode,
+				"validation-key": key,
+			}).Warn("Validator not found")
+
+			return nil, fmt.Errorf("Validator not found")
+		}
+
+		if res.StatusCode == 200 {
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				log.WithError(err).WithFields(log.Fields{
+					"url":            url,
+					"validation-key": key,
+				}).Warn("Unable to read rated network http body")
+
+				return nil, err
+			}
+
+			log.WithFields(log.Fields{
+				"url":            url,
+				"validation-key": key,
+			}).Info("Parsing response from rated network")
+
+			var response getValidationEffectiveness
+			err = json.Unmarshal(body, &response)
+			if err != nil {
+				log.WithError(err).WithFields(log.Fields{
+					"url":            url,
+					"validation-key": key,
+				}).Warn("Unable to parse rated network http body into expected response")
+
+				return nil, err
+			}
+
+			if len(response.Data) != 1 {
+				log.WithFields(log.Fields{
+					"url":            url,
+					"status-code":    res.StatusCode,
+					"validation-key": key,
+					"nb-entries":     len(response.Data),
+				}).Warn("Expected 1 entry of statistics for the validation key")
+
+				return nil, fmt.Errorf("unexpected data response from rated network")
+			} else {
+				log.WithFields(log.Fields{
+					"url":            url,
+					"validation-key": key,
+				}).Info("Successfully fetched statistics for validator")
+
+				return &response.Data[0], nil
+
+			}
+
+		}
+
+		if res.StatusCode == 429 {
+			min := 0.95
+			max := 1.05
+			sleepFor := math.Pow(2, float64(r)) * ((rand.Float64() * (max - min)) + min)
+
+			log.WithFields(log.Fields{
+				"url":         url,
+				"status-code": res.StatusCode,
+			}).Warn(fmt.Sprintf("Rate limit exceeded. Retrying in %v seconds.", sleepFor))
+
+			time.Sleep(time.Duration(sleepFor) * time.Second)
+
+		}
+
 	}
 
-	log.WithFields(log.Fields{
-		"url":            url,
-		"validation-key": key,
-	}).Info("fetched validator statistics about key from rated network")
+	return nil, fmt.Errorf(fmt.Sprintf("Failed to fetch Validator data after %v attempts", maxRetries))
 
-	return &response.Data[0], nil
 }
